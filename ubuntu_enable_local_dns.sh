7a91e8dceb5d2a8b8c7ce88f644719a30cbaa8ef
Daniel Rosel <daniel@alves.world>
2024-06-10 16:45:39 +0200

    enable ubuntu local dns

---
+++ ubuntu_enable_local_dns.sh
@@ -0,0 +1,10 @@
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+config=$(cat /etc/NetworkManager/NetworkManager.conf)
+if [[ $config == *dns=default* ]]; then
+    line_number=$(grep -n "\[main\]" /etc/NetworkManager/NetworkManager.conf | cut -d: -f1)
+    sed -i "$line_number a dns=default" /etc/NetworkManager/NetworkManager.conf
+fi
+cp /etc/resolv.conf /etc/resolv.conf.bak
+rm /etc/resolv.conf
+sudo systemctl restart NetworkManager
